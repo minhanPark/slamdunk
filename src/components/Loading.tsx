4b3d35f73fec674c59099815ad31d53524c20810
Running water <alsgks1218@naver.com>
2022-12-10 14:53:16 +0900

    install styled-components

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import SlamDunkText from "./SlamDunkText";
+import styled from "styled-components";
+
+const Background = styled.div`
+  background-color: "#0d1821";
+  height: "100vh";
+  display: "flex";
+  justify-content: "center";
+  align-items: "center";
+`;
 
 const Loading = () => {
   return (
-    <div
-      style={{
-        backgroundColor: "#0d1821",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <Background>
       <SlamDunkText />
-    </div>
+    </Background>
   );
 };
 
